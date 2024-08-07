c5bc7143366b6bdd31284a21907a7ffd177921f1
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-08-07 20:19:56 +0100

    Update User.java

--- src/main/java/com/persitance/entities/User.java
+++ src/main/java/com/persitance/entities/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class User {
@@ -13,6 +14,7 @@ public class User {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private long id;
 @Column(length = 30, nullable = false)
+@NotBlank
 private String nom;
 @Email
 @Column(nullable = false,unique = true)
